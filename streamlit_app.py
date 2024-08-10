10fbc898bf222ee5130f189d8481519968c1741a
Pei Ying <u0251077@gmail.com>
2024-08-11 02:57:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ if api_key:
                 model="gpt-4o-mini",
                 messages=[
                     {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": "从下面的句子中提取出关键概念和它们之间的关系："+text}
+                    {"role": "user", "content": "从下面的句子中提取出節點（nodes）和它们之间的關係（relationships）："+text}
                 ],
                 max_tokens=15000
             )
